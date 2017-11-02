d44756633578d5eba69b34c0968f262f4879556e
Jakub Kowalczyk <j.a.kowalczyk1993@gmail.com>
2017-11-02 22:28:27 +0000

    Add silent stop

--- tools/start.sh
+++ tools/start.sh
@@ -3,7 +3,9 @@
 # get environment variables
 # source /etc/profile.d/poland20.sh
 # use dotenv from server
+
 cp /var/www/.env ./.env
-npm install --only=production
+echo "Install dependencies"
+npm install
 npm run build
 npm run start
